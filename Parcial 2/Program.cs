3d6238e3883de9e0a68be90f07794f80dbae4ddf
BrayanSosaAlberto <125327983+BrayanSosaAlberto@users.noreply.github.com>
2023-04-18 20:47:58 -0600

    parcial 2

---
+++ Parcial 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Pregunta Parcial 2
+
+int datol = 16;
+string dato2 = "25g";
+
+try
+{
+    int resultado = datol + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("Error, no se pudo realizar corectamente la operacion que solicito");
+}
